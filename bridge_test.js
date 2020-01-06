2da4f3c26be775e968971c2525ebe28abd318ca7
Roland Schemers <roland.schemers@gmail.com>
2020-01-05 16:23:09 -0800

    checkpoint working getConfig

--- bridge_test.js
+++ bridge_test.js
@@ -1,11 +1,12 @@
 const Bridge = require('./lib/Bridge')
+const util = require('util')
 
 let controller = new Bridge.Controller({ ip_address: '192.168.86.143' })
 
 controller
   .getConfig()
   .then(config => {
-    console.log('got config:', config)
+    console.log('got the config:', util.inspect(config, { depth: 10 }))
   })
   .catch(err => {
     console.log('caught error:', err)
